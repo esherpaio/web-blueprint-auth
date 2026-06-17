b48f21d0d1558dccdefac30d04ca919db614588d
Stan Mertens <stnmertens@gmail.com>
2026-06-17 11:50:57 +0700

    repo alignment

--- bp_auth/__init__.py
+++ bp_auth/__init__.py
@@ -1,11 +1,2 @@
 from .blueprint import auth_bp, auth_static_jobs
 from .routes import login, password_recover, password_request, register
-
-__all__ = [
-    "auth_bp",
-    "auth_static_jobs",
-    "login",
-    "password_recover",
-    "password_request",
-    "register",
-]
